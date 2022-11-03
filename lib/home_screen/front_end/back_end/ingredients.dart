391eb79d4186c854ac81de2d4d42a6b5e0661b74
sanidhya2510 <sanidhya2510@gmail.com>
2022-11-03 21:36:04 +0530

    adds ingredients page

---
+++ lib/home_screen/front_end/back_end/ingredients.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+List<String> ingredients =[];
+class FirestoreDB {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<List<String>> getData() async {
+    QuerySnapshot<Map<String, dynamic>> qr =
+    await FirebaseFirestore.instance.collection('ingredients').get();
+    List<String> str = [];
+    for (var element in qr.docs) {
+      final m = element.data();
+      str.add(m['Name']);
+    }
+    ingredients = str;
+
+    return str;
+  }
+
+}
