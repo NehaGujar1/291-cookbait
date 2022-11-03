4242886ed6bead4573bf27961b3f7d4919af9874
Raiwat Bapat <101309747+raiwat7@users.noreply.github.com>
2022-11-03 18:30:41 +0530

    Update backEnd.dart

--- lib/commentSection/backEnd.dart
+++ lib/commentSection/backEnd.dart
@@ -3,21 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FireBaseCollectionOfComments {
   final FirebaseFirestore fireStore = FirebaseFirestore.instance;
 
-  // Future<List<String>> getData() async {
-  //   QuerySnapshot<Map<String, dynamic>> qr =
-  //   await FirebaseFirestore.instance.collection('comments').get();
-  //   List<String> str=[];
-  //   for (var element in qr.docs) {
-  //     final m = element.data();
-  //     str.add(m['data']);
-  //   }
-  //   return str;
-  // }
-
   Future<void> addData(String? name, String? email, String? comment, String? recipeID) async {
-    // await FirebaseFirestore.instance
-    //     .collection('comments').doc('1001')
-    //     .update({'Raiwat': 'hello2'});
     bool didIUpdateTheCommentCollection = false;
     QuerySnapshot<Map<String, dynamic>> collectionOfDocuments =
        await FirebaseFirestore.instance.collection('comments').get();
@@ -25,11 +11,7 @@ class FireBaseCollectionOfComments {
       if (element.id == recipeID) {
         Map<String?, dynamic> listOfFields = element.data();
         for (int i = 0; i < listOfFields.length; i++) {
-          //print(listOfFields[i.toString()][0]);
           if (listOfFields[i.toString()][0] == email) {
-            // String? fuck = listOfFields[i.toString()].toString();
-            // print(fuck);
-            //final data = {i.toString() : comment};
             final documentToBeUpdated = FirebaseFirestore.instance.
             collection('comments').
             doc(recipeID);
@@ -39,9 +21,6 @@ class FireBaseCollectionOfComments {
         }
         if (!didIUpdateTheCommentCollection) {
           List<String?> newArray = [email, name, comment];
-          // listOfFields.update(listOfFields.length.toString(), (value) => newArray);
-          // listOfFields[(listOfFields.length - 1).toString()].add(name);
-          // listOfFields[(listOfFields.length - 1).toString()].add(comment);
           final documentToBeUpdated = FirebaseFirestore.instance.
           collection('comments').
           doc(recipeID);
@@ -53,7 +32,6 @@ class FireBaseCollectionOfComments {
     }
     if (!didIUpdateTheCommentCollection) {
       List<String?> newArray = [email, name, comment];
-      // await FirebaseFirestore.instance.collection('comments').doc(recipeID).update({'0': newArray});
       final newDocument = FirebaseFirestore.instance.
       collection('comments').
       doc(recipeID);
